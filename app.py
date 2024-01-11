4f55195444509762264ae44c43dc221da98fa875
PonchTwitch <94783288+PonchTwitch@users.noreply.github.com>
2024-01-11 19:47:13 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -14,15 +14,20 @@ def calculate():
         if not x1 or not x1.isdigit() or not x2 or not x2.isdigit() or not x3 or not x3.isdigit():
             return render_template('index.html', error='Все поля должны быть заполнены числами')
 
-        # Выполняем расчет
-        result = 2 * int(x1) + int(x2) * int(x3)
+        # Преобразуем значения в числа
+        x1 = int(x1)
+        x2 = int(x2)
+        x3 = int(x3)
+
+        # Сортируем числа в порядке возрастания
+        numbers = [x1, x2, x3]
+        numbers.sort()
 
         # Отображаем результат
-        return render_template('result.html', result=result)
+        return render_template('result.html', numbers=numbers)
 
     # Если метод GET, отображаем пустую форму
     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
-    
